a7df9021ec81130e573ddd6746fb97e423ae30b3
Soumyadeep Chakravrti <soumyadeepsai1@gmail.com>
2026-09-04 15:34:40 +0530

    feat(git): add portable defaults and command aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -14,11 +14,7 @@ alias cat='bat --paging=never'
 
 # Git
 alias g='git'
-alias gs='git status'
-alias ga='git add'
-alias gc='git commit'
-alias gp='git push'
-alias gl='git log --oneline --graph --decorate'
+
 
 # Neovim
 alias v='nvim'
